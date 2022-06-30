01d84f30997818e58d722a8ec96eccade0439239
nathangobinet <nathan.gobinet@gmail.com>
2022-06-30 16:43:14 -0400

    Apply container & padding to all

--- 404.php
+++ 404.php
@@ -10,7 +10,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main container main-padding-y">
 
 		<section class="error-404 not-found">
 			<header class="page-header">
